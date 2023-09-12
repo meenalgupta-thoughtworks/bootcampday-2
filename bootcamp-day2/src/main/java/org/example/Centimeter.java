07ae3dca45d117445f7fd3955a4ae6133f3f62c7
meenalgupta-thoughtworks <144127824+meenalgupta-thoughtworks@users.noreply.github.com>
2023-09-12 10:10:03 +0530

    [Meenal | Pallavi] Add. Centimeter Class

---
+++ bootcamp-day2/src/main/java/org/example/Centimeter.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Centimeter {
+
+    private final int magnitude;
+    private final String unit;
+
+    public Centimeter(int magnitude, String unit) {
+        this.magnitude = magnitude;
+        this.unit = unit;
+    }
+
+
+}
---
+++ bootcamp-day2/src/test/java/org/example/CentimeterTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CentimeterTest {
+
+    @Test
+    public void showEqualIfCentimeterIsEqualTest() {
+        Centimeter centimeter = new Centimeter(1, "cm");
+    }
+
+}
