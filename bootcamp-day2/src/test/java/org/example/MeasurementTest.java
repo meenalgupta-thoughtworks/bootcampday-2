b3ed576d475a1964c5e5accd969691c07596c5db
meenal.gupta <meenal.gupta@thoughtworks.com>
2023-09-12 12:05:38 +0530

    Delete. Deleted the CentimeterTest class

--- bootcamp-day2/src/test/java/org/example/MeasurementTest.java
+++ bootcamp-day2/src/test/java/org/example/MeasurementTest.java
@@ -7,6 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class MeasurementTest {
 
     @Test
+    public void showEqualIfCentimeterIsEqualToOtherCentimeterTest() {
+
+        final Measurement c1 = Measurement.createCentimeter(100);
+        final Measurement c2 = Measurement.createCentimeter(100);
+        assertEquals(c1, c2);
+    }
+
+    @Test
+    public void showEqualIfCentimeterIsSameToOtherCentimeterTest() {
+        final Measurement c1 = Measurement.createCentimeter(100);
+        assertEquals(c1, c1);
+    }
+
+    @Test
+    public void showNonEqualIfCentimeterIsEqualToOtherCentimeterTest() {
+        final Measurement c1 = Measurement.createCentimeter(10);
+        final Measurement c2 = Measurement.createCentimeter(100);
+        assertFalse(c1.equals(c2));
+    }
+
+    @Test
     public void showEqualIf1MeterIsEqualTo100Centimeters() {
         final Measurement c = Measurement.createCentimeter(100);
         final Measurement m = Measurement.createMeter(1);
